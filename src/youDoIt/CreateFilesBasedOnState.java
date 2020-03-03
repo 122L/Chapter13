8be83cc39f466b3b45c2d90656ba980565d5d62a
122L <og153182@students.davenport.k12.ia.us>
2020-03-03 14:00:09 -0600

    bruh

---
+++ src/youDoIt/CreateFilesBasedOnState.java
@@ -0,0 +1,50 @@
+package youDoIt;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedWriter;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.nio.channels.FileChannel;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+import static java.nio.file.StandardOpenOption.CREATE;
+
+public class CreateFilesBasedOnState {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Path inStateFile = Paths.get("C:\\Users\\og153182\\Desktop\\Java\\Chapter13\\src\\youDoIt\\InStateCusts.txt");
+        Path outOfStateFile = Paths.get("C:\\Users\\og153182\\Desktop\\Java\\Chapter13\\src\\youDoIt\\OutOfStateCusts.txt");
+        final String ID_FORMAT = "000";
+        final String NAME_FORMAT = "    ";
+        final int NAME_LENGTH = NAME_FORMAT.length();
+        final String HOME_STATE = "IA";
+        final String BALANCE_FORMAT = "0000.00";
+        String delimiter = ",";
+        String s = ID_FORMAT + delimiter + NAME_FORMAT + delimiter + HOME_STATE + delimiter + BALANCE_FORMAT +
+                System.getProperty("line.separator");
+        final int RECSIZE = s.length();
+        FileChannel fcIn = null;
+        FileChannel fcOut = null;
+        String idString;
+        int id;
+        String name;
+        String state;
+        double Balance;
+        final String QUIT = "999";
+        createEmptyFile(inStateFile, s);
+        createEmptyFile(outOfStateFile, s);
+    }
+    public static void createEmptyFile(Path file, String s) {
+        final int NUMRECS = 1000;
+        try {
+            OutputStream output = new BufferedOutputStream(Files.newOutputStream(file, CREATE));
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(output));
+        }
+        catch (Exception e) {
+            System.out.println("Error message: " + e);
+        }
+    }
+}
